48e38320effb989ec95988e2f2834b97148bb8f2
Olivier Gagnon-Simard <oliviergagnonsimard2@gmail.com>
2025-11-29 15:27:04 -0500

    initial

---
+++ tp5.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(require "Flots-lib.rkt")
+(require "TP5-lib-facturation.rkt")
+(require "TP5-lib-charge.rkt")
+
+; ***************** IFT359 / TP5
+; ***************** Gagnon-Simard, Olivier (24096336)
+
+(define (bill-one stream-of-reports month company)
+  (print "beans")
+  )
